1d76f5a615d32c2852f44651aaa7ea30d060cb90
xinghunm <xinghunm@curacloudcorp.com>
2019-10-11 18:31:47 +0800

    add electron

---
+++ electron/main.js
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// Modules to control application life and create native browser window
+const electron = require('electron');
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  const { width, height } = electron.screen.getPrimaryDisplay().size; // eslint-disable-line
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width,
+    height,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preloads', 'main-preload.js')
+    }
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:1989');
+  } else {
+    mainWindow.loadURL(
+      `file://${path.join(app.getAppPath(), 'build', 'index.html')}`
+    );
+  }
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  app.quit();
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow();
+});
---
+++ electron/preloads/main-preload.js
@@ -0,0 +1,6 @@
+Object.defineProperty(window, 'Electron', {
+  configurable: false,
+  enumerable: false,
+  writable: false,
+  value: Object.freeze({})
+});
